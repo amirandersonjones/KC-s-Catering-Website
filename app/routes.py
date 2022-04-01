52ad3b84fc54a6236fb454a24ee05102f2479834
amirandersonjones <amirandersonjones@gmail.com>
2022-04-01 17:17:17 -0500

    more

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,18 @@
 from app import app
 
 from flask import render_template
+#non-flask import for route functionality
+from .services import getDrinkRecs #need to put .services so it wont be looking for a python module. This is a services file and not a python module.
 
 #this is the base url
 @app.route('/')
 @app.route('/kcscateringhomepage') #decorator says this is a route of the flask app 'app' with the url endpoint '/marvelhomepage'
 def home():
-    return render_template('index.html')
+    drinkrecs = getDrinkRecs()
+    return render_template('index.html', drinkrecs=drinkrecs)
 
+@app.route('/kcscateringphotos')
+def photos():
+    return render_template('photos.html')
+
+@app.route('')
---
+++ app/services.py
@@ -0,0 +1,23 @@
+#additional classes and functions put my function definition in here.
+# We don't need the function call here.
+
+#we must install the requests package in our venv to use it
+from re import X
+import requests as r
+from unicodedata import name
+import requests as r
+
+def getDrinkRecs():
+    response = r.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
+    if response.status_code == 200:
+        data = response.json()
+    else: 
+        return response.status_code 
+    drinkrecs = []
+    for data in data['drinks']:
+        if data['strDrinkThumb']:
+            drinkrecs.append((data['strDrink'], data['strDrinkThumb']))
+            # print(data['strDrink'], data['strDrinkThumb'])
+            # print(drinkrecs)
+    
+    return drinkrecs
